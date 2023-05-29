ea7ad16b95cd940aa8dd3082b39bea449a5c168d
mbombka <mbombka@gmail.com>
2023-05-29 15:39:50 +0200

    Working projectile shooting

--- application/src/main/java/part14/AsteroidsApplication.java
+++ application/src/main/java/part14/AsteroidsApplication.java
@@ -5,12 +5,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class AsteroidsApplication extends Application{
@@ -22,18 +25,25 @@ public class AsteroidsApplication extends Application{
       public void start(Stage stage) {   
         
         Pane pane = new Pane();
+        Text text = new Text(10, 20, "Points: 0");
+        pane.getChildren().add(text);
+
+        AtomicInteger points = new AtomicInteger();
+
         pane.setPrefSize(WIDTH, HEIGHT);
 
         Ship ship = new Ship(WIDTH / 2, HEIGHT / 2);
+        
         List<Asteroid> asteroids = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             Random rnd = new Random();
             Asteroid asteroid = new Asteroid(rnd.nextInt(WIDTH / 3), rnd.nextInt(HEIGHT));
             asteroids.add(asteroid);
         }
-
         pane.getChildren().add(ship.getCharacter());
         asteroids.forEach(asteroid -> pane.getChildren().add(asteroid.getCharacter()));
+
+        List<Projectile> projectiles = new ArrayList<>(); 
         
         Scene scene = new Scene(pane);
 
@@ -53,6 +63,8 @@ public class AsteroidsApplication extends Application{
 
             @Override
             public void handle(long now) {
+                
+
                 if(pressedKeys.getOrDefault(KeyCode.LEFT, false)) {
                     ship.turnLeft();
                 }
@@ -63,15 +75,67 @@ public class AsteroidsApplication extends Application{
                 if(pressedKeys.getOrDefault(KeyCode.UP, false)) {
                     ship.accelerate();
                 }
-
+                
                 ship.move();
-                asteroids.forEach(asteroid -> asteroid.move());
+                asteroids.forEach(asteroid -> asteroid.move());                
+                projectiles.forEach(projectile -> projectile.move());
+                        
+
+                projectiles.forEach(projectile -> {
+                    asteroids.forEach(asteroid -> {
+                        if(projectile.collide(asteroid)) {
+                            projectile.setAlive(false);
+                            asteroid.setAlive(false);
+                        }
+                    });
+                    if(!projectile.isAlive()) {
+                        text.setText("Points: " + points.addAndGet(1000));                       
+                    }
+                });
+                
+                projectiles.stream()
+                    .filter(projectile -> !projectile.isAlive())
+                    .forEach(projectile ->{
+                        pane.getChildren().remove(projectile.getCharacter());
+                        System.out.println("projectile removed + " + projectile.getCharacter().toString());
+                    } );
+                projectiles.removeAll(projectiles.stream()
+                        .filter(projectile -> !projectile.isAlive())
+                        .collect(Collectors.toList()));
+
+                asteroids.stream()
+                        .filter(asteroid -> !asteroid.isAlive())
+                        .forEach(asteroid -> pane.getChildren().remove(asteroid.getCharacter()));   
+                asteroids.removeAll(asteroids.stream()
+                        .filter(asteroid -> !asteroid.isAlive())
+                         .collect(Collectors.toList()));                
+                
 
                 asteroids.forEach(asteroid -> {
                     if (ship.collide(asteroid)) {
                         stop();
                     }
                 });
+
+                if (pressedKeys.getOrDefault(KeyCode.SPACE, false)) {
+                    // we shoot
+                    Projectile projectile = new Projectile((int) ship.getCharacter().getTranslateX(), (int) ship.getCharacter().getTranslateY());
+                    projectile.getCharacter().setRotate(ship.getCharacter().getRotate());
+                    projectiles.add(projectile);
+
+                    projectile.accelerate();
+                    projectile.setMovement(projectile.getMovement().normalize().multiply(3));
+
+                    pane.getChildren().add(projectile.getCharacter());
+                }
+
+                if(Math.random() < 0.005) {
+                    Asteroid asteroid = new Asteroid(WIDTH, HEIGHT);
+                    if(!asteroid.collide(ship)) {
+                        asteroids.add(asteroid);
+                        pane.getChildren().add(asteroid.getCharacter());
+                    }
+                }
             }
         }.start();
 
@@ -86,7 +150,7 @@ public class AsteroidsApplication extends Application{
 
     public static int partsCompleted() {
         // State how many parts you have completed using the return value of this method
-        return 0;
+        return 4;
     }
 
 
